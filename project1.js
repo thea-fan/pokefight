1c3458cf9dc4d322de2aaba1f676bcaa95a9357a
thea-fan <julythefourteen@gmail.com>
2019-07-09 23:15:03 +0800

    game ready for submission

--- project1.js
+++ project1.js
@@ -4,19 +4,23 @@ console.log(`*****************`)
 var userScore = 0;
 var comScore = 0;
 var turn = 0;
-var totalRound = 5;
+var totalRound = 10;
 var userArr = [];
 var heartIndex = 0;
+var timer= document.querySelector('#timer');
 var monsterURL = ["https://img.pokemondb.net/artwork/ivysaur.jpg", "https://img.pokemondb.net/artwork/ponyta.jpg","https://img.pokemondb.net/artwork/vaporeon.jpg","https://img.pokemondb.net/artwork/marowak.jpg","https://img.pokemondb.net/artwork/golem.jpg"]
 var monsterList = ["Ivysaur", "Ponyta", "Vaporeon", "Marowak","Golem"];
 var monsterType = ["grass", "fire", "water", "ground", "rock"];
 var typeURL = ["assets/typegrass.png","assets/typefire.png","assets/typewater.png","assets/typeground.png","assets/typerock.png"];
 var enemyHealth = document.getElementById("enemyHealth");
+var userHealth = document.getElementById("userHealth");
 var attack_id = document.getElementById("attack");
+var playerWin_id = document.getElementById("playerWin");
+var enemyWin_id = document.getElementById("enemyWin");
 var userScore_id = document.getElementById("user-score");
 var comScore_id = document.getElementById("com-score");
 var result_id = document.getElementById("result");
-var monster_id = document.getElementById("monster");
+var enemy_id = document.getElementById("enemy");
 var user_id = document.getElementById("user");
 var fire_id = document.getElementById("fire");
 var water_id = document.getElementById("water");
@@ -34,25 +38,61 @@ var monster= function(){
     currentMonster = monsterType[monsterIndex];
 
     console.log("The enemy is: "+currentMonster);
-    document.getElementById("enemy").setAttribute("src", monsterURL[monsterIndex]);
+    enemy_id.setAttribute("src", monsterURL[monsterIndex]);
     document.getElementById("type").setAttribute("src", typeURL[monsterIndex]);
+    setTimeout(function(){ enemy_id.classList.remove('explode');}, 300);
+    setTimeout(function(){ playerWin_id.classList.add('toHide');}, 400);
 }
+
+//Animation*******************
+function animateCSS(element, animationName, callback) {
+    const node = document.querySelector(element)
+    node.classList.add('animated', animationName)
+
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName)
+        node.removeEventListener('animationend', handleAnimationEnd)
+
+        if (typeof callback === 'function') callback()
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd)
+}
+
 
 
 
 //Game outcome****************************
 var userWin = function (){
-    result_id.textContent = "You beat the monster!"}
+    result_id.textContent = "You beat the enemy!"}
 
 var comWin = function (){
-    result_id.textContent = "Uh oh..the monster has won!"}
+    result_id.textContent = "Uh oh..you have lost!"}
 
 var gameDraw = function (){
     result_id.textContent = "It's a draw!"}
 
 var checkWin = function(){
     if (turn < totalRound){
-        monster() //%%%%%%% To set delay
+        if (enemyHealth.value <= 0){
+        enemy_id.classList.add('explode');
+        userScore++;
+        userScore_id.textContent++;
+        playerWin_id.classList.remove('toHide');
+        monster();
+        setTimeout(function(){ enemyHealth.value =10; }, 500);
+
+        } else if (userHealth.value <= 0){
+            comScore++;
+            comScore_id.textContent++;
+            enemyWin_id.classList.remove('toHide');
+            setTimeout(function(){ enemyWin_id.classList.add('toHide');}, 400);
+            var fullHeart = document.querySelector(".full")
+            fullHeart.classList.add('is-empty');
+            fullHeart.classList.remove('full');
+            setTimeout(function(){ userHealth.value =10; }, 500);}
+            monster();
+
     } else if (turn === totalRound && userScore>comScore){
         userWin();
     } else if (turn === totalRound && comScore>userScore){
@@ -63,61 +103,101 @@ var checkWin = function(){
 }
 
 
-//Game logic****************************
-var play = function (x){
 
+//Damage extent***************************
+var totalKillEnemy = function (){
+    animateCSS('#enemyHealth', 'heartBeat');
+    enemyHealth.value -=10;
+}
+
+var halfKillEnemy = function (){
+    animateCSS('#enemy', 'jello');
+    animateCSS('#enemyHealth', 'heartBeat');
+    enemyHealth.value -=5;
+}
+
+var quarterKillEnemy = function (){
+    animateCSS('#enemyHealth', 'heartBeat');
+    animateCSS('#enemy', 'jello');
+    enemyHealth.value -=2.5;
+}
+
+var totalKillPlayer = function (){
+    animateCSS('.display', 'shake');
+    animateCSS('#userHealth', 'heartBeat');
+    userHealth.value -=10;
+}
+
+var halfKillPlayer = function (){
+    animateCSS('#userHealth', 'heartBeat');
+    userHealth.value -=5;
+}
+
+var quarterKillPlayer = function (){
+    animateCSS('#userHealth', 'heartBeat');
+    userHealth.value -=2.5;
+}
+
+
+
+
+//Game logic****************************
+var play = function (userInput){
 
     turn++;
 
+    console.log("User chose "+userInput);
+        user_id.textContent="";
 
-    var userInput = event.target.id;
-    console.log ('Start of Round '+turn);
-
-    console.log("User chose "+x);
-
-if (x === currentMonster){
+if (userInput === currentMonster){
     user_id.textContent = "No effect! Same type!";
 
 }else {
 
-    switch (x + currentMonster){
+    switch (userInput + currentMonster){
+    //Win scenarios
         case "waterfire":
-        case "waterground":
-        case "waterrock":
         case "firegrass":
+        case "grasswater":
         case "groundrock":
+        totalKillEnemy();
+        break
+
+        case "waterrock":
         case "groundfire":
+        case "grassground":
+        halfKillEnemy();
+        break
+
         case "rockfire":
         case "grassrock":
-        case "grassground":
-        case "grasswater":
-
-            enemyHealth.value -=10;
-            setTimeout(function(){ enemyHealth.value =10; }, 500);
-            userScore++;
-            userScore_id.textContent++;
+        case "waterground":
+        quarterKillEnemy();
         break
+
+    //Lose scenarios
         case "firewater":
-        case "groundwater":
-        case "rockwater":
         case "grassfire":
-        case "rockground":
-        case "fireground":
-        case "firerock":
-        case "rockgrass":
-        case "groundgrass":
         case "watergrass":
-
-            comScore++;
-            comScore_id.textContent++;
+        case "rockground":
+        totalKillPlayer();
         break
 
-        }
+        case "rockwater":
+        case "fireground":
+        case "groundgrass":
+        halfKillPlayer();
+        break
+
+        case "groundwater":
+        case "rockgrass":
+        case "firerock":
+        quarterKillPlayer();
+        break
+
+        } checkWin();
     }
 
-
-    console.log ('End of Round '+turn);
-    checkWin();
 
 }
 
@@ -125,35 +205,30 @@ if (x === currentMonster){
 //Send user end input: Attack button***********************
 var sendAttack = function(userInput){
 
+
     var userInput = event.target.id;
     userArr.push(userInput);
+    var selected = event.target;
+    selected.classList.add("selected");
+
     user_id.textContent="You use "+userArr;
 
     console.log(`sendAttack is: `+userArr);
     attack_id.addEventListener("click", function(){checkCombo(userArr)})
+
 
 }
 
 
 //Check combo****************************
 var combo1 = function(){
-    var fullHeart = document.querySelector(".full")
-    fullHeart.classList.add('is-empty');
-    fullHeart.classList.remove('full');
     var combo = userArr[0];
     play(combo);
 }
 
 
-//%%%%%%%%%% To code combo2
+//Combo2
 var combo2 = function(){
-    var fullHeart = document.querySelectorAll(".full")
-    for (var i=0; i<2; i++){
-        fullHeart[i].classList.add('is-empty');
-    };
-    for (var j=0; j<2; j++){
-        fullHeart[j].classList.remove('full');
-    };
     console.log(userArr);
     var combo = userArr[0]+userArr[1];
     console.log(combo);
@@ -161,15 +236,8 @@ var combo2 = function(){
 }
 
 
-//%%%%%%%%%% To code combo3
+//Combo3
 var combo3 = function(){
-    var fullHeart = document.querySelectorAll(".full")
-    for (var i=0; i<3; i++){
-        fullHeart[i].classList.add('is-empty');
-    };
-    for (var j=0; j<3; j++){
-        fullHeart[j].classList.remove('full');
-    };
     console.log(userArr);
     var combo = userArr[0]+userArr[1]+userArr[2];
     console.log(combo);
@@ -179,8 +247,11 @@ var combo3 = function(){
 
 var checkCombo = function (userInput){
 
+    animateCSS('#attack', 'shake');
+    var selectedPowers = document.querySelectorAll(".selected");
+    for (var i = 0; i < selectedPowers.length; i++) {
+    selectedPowers[i].classList.remove("selected")}
 
-    user_id.textContent="";
     switch(userArr.length){
     case 1:
         combo1();
@@ -214,10 +285,48 @@ var setUp = function(){
     grass_id.addEventListener("click", sendAttack);
     rock_id.addEventListener("click", sendAttack);
     attack_id.addEventListener("click", alertInput);
+    playerWin.classList.add('toHide');
+    enemyWin.classList.add('toHide');
 
     setTimeout(function(){monster(); }, 500);
-
-
 }
 
 setUp();
+
+//Lose Game****************************
+var alertLoseGame = function (){
+    document.querySelector('body').style.backgroundBlendMode="difference";
+    document.querySelector('body').style.color="white";
+    document.querySelector('#timer').style.border="5px solid red";
+}
+
+
+
+//Timer****************************
+function startTimer(duration, display) {
+    var time = duration, seconds;
+    setInterval(function () {
+        seconds = parseInt(time % 60, 10);
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        timer.textContent = "Time: 0:" + seconds;
+
+        //Warning: last 10 sec
+        if (seconds === 10){
+        alertLoseGame();
+        }
+
+        //No more time
+        if (--time < 0) {
+            timer.textContent = "Time ran out!";
+            alert('Sorry try again!')
+        }
+
+    }, 1000);
+
+}
+
+window.onload = function () {
+    var thirtySec = 60 / 2;
+    startTimer(thirtySec, timer);
+};
